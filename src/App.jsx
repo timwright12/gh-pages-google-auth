809bac3f3d13e1b13ea7198d7be2495bcdd998a0
Tim Wright <timwright12@gmail.com>
2025-06-06 16:13:00 -0400

    updated redirect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Welcome from './Welcome';
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='gh-pages-google-auth'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/welcome" element={<Welcome />} />
